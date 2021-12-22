85bdd3b65e9951046d109ff29c94d5e1fca6a3f9
Alex <61236755+SeclaPanda@users.noreply.github.com>
2021-12-22 14:11:48 +0300

    New branch. Second part of course

--- app/src/main/java/com/example/netvoicassist/MainActivity.kt
+++ app/src/main/java/com/example/netvoicassist/MainActivity.kt
@@ -5,31 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-
-fun main(): String {
-    var name = "Ivan"
-    var surname = "Ivanov"
-    var age = 37
-    var height = 172.2
-
-    val sum = "name: $name surname: $surname age: $age height: $height"
-    return sum
-}
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val output: TextView = findViewById(R.id.output)
-        output.text = main()
-
-        val sum = main()
-        Log.d(TAG, sum)
-
-        var button: Button = findViewById(R.id.button)
-
+        initViews()
     }
+    fun initViews() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
     //add comment to check work of git
 }
